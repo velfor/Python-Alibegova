8729321d7c83a5b49fc53be5702d6e1a164ef0d6
Paul Nikiforoff <velfor@tut.by>
2022-01-12 18:51:50 +0300

    Add files via upload

--- Pong/Pong1.py
+++ Pong/Pong1.py
@@ -1,77 +1,89 @@
-import pygame
+import pygame as pg
 import sys
 from const import *
-from platform import Platform
 from ball import Ball
 
-# создание окна программы
-screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-#создаем таймер для управления скоростью игры
-clock = pygame.time.Clock()
-
-# создаем игровые объекты
-# переменные для сторон
-a = 10
-b = 80
-left_paddle = Platform(20,SCREEN_HEIGHT // 2 - b//2)
-right_paddle = Platform(SCREEN_WIDTH - 20 - a, SCREEN_HEIGHT // 2 - b//2)
+#создаем окно приложения
+screen = pg.display.set_mode((SC_WIDTH,SC_HEIGHT))
+pg.display.set_caption("My little Pong")
+#0 секция - создание игровых объектов
+# часы для контроля скорости игры
+clock = pg.time.Clock()
+#создаем мяч
 ball = Ball()
-# переменные для счета
+# создаем шрифт
+pg.font.init()
+font = pg.font.SysFont('arial',28)
+# для прямоугольника 4 переменные:
+# первые 2 - координаты верхнего левого угла
+# вторые 2 - длина и ширина
+r_width = 20
+r_height = 80
+r1_x = r_width
+r1_y = SC_HEIGHT//2 - r_height//2
+r2_x = SC_WIDTH - 2*r_width
+r2_y = SC_HEIGHT//2 - r_height//2
+r_speedy = 5
 score1 = 0
 score2 = 0
-#инициализация подмодуля font
-pygame.font.init()
-# создаем системный шрифт (имя шрифта, размер)
-font = pygame.font.SysFont('Arial',36)
 
-# основной игровой цикл
+# бесконечный игровой цикл
 while True:
-    #№ 1 - обработка игровых событий (ввод пользователя)
-    for event in pygame.event.get():
-        #если тип события - ВЫХОД
-        if event.type == pygame.QUIT:
-            #остановить модули библиотеки
-            pygame.quit()
-            #закрыть окно программы
+    # 1 секция - обработка ввода пользователя
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
             sys.exit()
-        # если тип события - КЛАВИША НАЖАТА
-##        if event.type == pygame.KEYDOWN:
-##            #если нажата стрелка вниз
-##            if event.key == pygame.K_DOWN:
-##                #увеличиваем координату у прямоугольника
-##                r_y += r_speedy
-##            if event.key == pygame.K_UP:
-##                r_y -= r_speedy
-   
-    #№ 2 - изменение состояния игровых объектов
-    # =====КРУГ=====
+    # 2 секция - обновление игровых объектов, update
     ball.update()
-    #управление с клавиатуры левой ракеткой
-    left_paddle.update(pygame.K_DOWN,pygame.K_UP)
-    #управление с клавиатуры правой ракеткой
-    right_paddle.update(pygame.K_s,pygame.K_w)
-
-    #=====Отражение мяча от левой ракетки=====
-    if (ball.x - ball.r <= left_paddle.x + a) and (ball.y + ball.r >= left_paddle.y) and
-    (ball.y - ball.r <= left_paddle.y + b):
-        ball.speedx = -ball.speedx
-    #=====Отражение мяча от правой ракетки=====    
-    if (ball.x + ball.r >= right_paddle.x) and (ball.y + r >= right_paddle.y) and 
-    (ball.y - ball.r <= right_paddle.y + b):
-        ball.speedx = -ball.speedx
-    # текст для счета левой ракетки
-    score1_text = font.render(str(score1),True,WHITE)
-    # текст для счета правой ракетки
-    score2_text = font.render(str(score2),True,WHITE)
-    # № 3 - перерисовка экрана
-    screen.fill(BLACK)
-    ball.draw(screen)
-    left_paddle.draw(screen)
-    right_paddle.draw(screen)
-    screen.blit(score1_text, (100,50))
-    screen.blit(score2_text, (SCREEN_WIDTH - 100,50))
-    pygame.display.flip()
     
+    # надписи для счетов
+    score1_text = font.render(str(ball.score1),True,WHITE)
+    score2_text = font.render(str(ball.score2),True,WHITE)
+    
+    keys = pg.key.get_pressed()
+    #движение правой ракетки по клавишам-стрелкам
+    if keys[pg.K_UP]:
+        r2_y -= r_speedy
+    elif keys[pg.K_DOWN]:
+        r2_y += r_speedy
+    #не даём выйти правой ракетке за границы экрана
+    if r2_y <= 0:
+        r2_y = 0
+    if r2_y >= SC_HEIGHT - r_height:
+        r2_y = SC_HEIGHT - r_height
+    #движение левой ракетки по клавишам-стрелкам
+    if keys[pg.K_w]:
+        r1_y -= r_speedy
+    elif keys[pg.K_s]:
+        r1_y += r_speedy    
+    #не даём выйти левой ракетке за границы экрана
+    if r1_y <= 0:
+        r1_y = 0
+    if r1_y >= SC_HEIGHT - r_height:
+        r1_y = SC_HEIGHT - r_height
+
+    #мяч попал в левую ракетку (только передняя поверхность)
+    if (ball.y >= r1_y and ball.y <= r1_y + r_height) and (
+        ball.x - ball.radius <= r1_x + r_width):
+        ball.speedx = -ball.speedx
+    #мяч попал в правую ракетку
+    if (ball.y >= r2_y and ball.y <= r2_y + r_height) and (
+        ball.x + ball.radius >= r2_x):
+        ball.speedx = -ball.speedx
         
-    # задержка, чтобы скорость игры была постоянной на любых компах
+    # 3 секция - перерисовка экрана
+    #3.1 залить экран цветом фона (или вывести картинку фона)
+    screen.fill(BLACK)
+    #3.2 нарисовать объекты
+    pg.draw.rect(screen,WHITE, (r1_x,r1_y,r_width,r_height))
+    pg.draw.rect(screen,WHITE, (r2_x,r2_y,r_width,r_height))
+    #pg.draw.circle(screen,YELLOW,(c_x,c_y),radius)
+    ball.draw(screen)
+    screen.blit(score1_text,(100,10))
+    screen.blit(score2_text,(SC_WIDTH-120,10))
+    #3.3 обновление экрана
+    pg.display.flip()
+
+    # задержка для постоянной скорости игры на разных компах
     clock.tick(FPS)
--- Pong/ball.py
+++ Pong/ball.py
@@ -1,29 +1,31 @@
+import pygame as pg
 from const import *
-import pygame
 
 class Ball:
     def __init__(self):
-        self.x = SCREEN_WIDTH // 2
-        self.y = SCREEN_HEIGHT // 2
-        self.speedy = 2
+        self.x = SC_WIDTH // 2
+        self.y = SC_HEIGHT // 2
+        self.radius = 10
         self.speedx = 3
-        self.r = 10
-    
+        self.speedy = 3
+        self.score1 = 0
+        self.score2 = 0
+
     def update(self):
+        #движение мяча
         self.x += self.speedx
         self.y += self.speedy
-
-        # отталкивание правой границы, очко получает левая (первая) ракетка
-        if self.x + self.r > SCREEN_WIDTH:
-            score1 += 1
+        # отражение шара от левой границы, +1 очко 2-му игроку
+        if self.x - self.radius <= 0:
+            self.score2 += 1
             self.speedx = -self.speedx
-        # отталкивание левой границы, очко получает правая (вторая) ракетка
-        if self.x - self.r < 0:
-            score2 += 1
-            self.speed_x = -self.speedx
-        # отталкивание от нижней и верхней границ
-        if self.y + self.r > SCREEN_HEIGHT or self.y - self.r < 0:
+        # отражение шара от правой границы, +1 очко 1-му игроку  
+        if self.x + self.radius >= SC_WIDTH:
+            self.score1 += 1
+            self.speedx = -self.speedx
+        #отражение от верхней или нижней границы
+        if self.y - self.radius <= 0 or self.y + self.radius >= SC_HEIGHT:
             self.speedy = -self.speedy
-    
+
     def draw(self,screen):
-        pygame.draw.circle(screen, YELLOW, (self.x, self.y), self.r)
+        pg.draw.circle(screen,YELLOW,(self.x,self.y),self.radius)
